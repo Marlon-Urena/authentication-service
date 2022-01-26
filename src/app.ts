1283d6e2783422fe1721d5c9f2e939b3c01d31ad
Marlon-Urena <anewryurena@gmail.com>
2022-01-26 14:20:06 +0000

    added origin environment variable

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ initializeApp({
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://localhost:3000'],
+  origin: ['http://localhost:3000', 'https://localhost:3000', process.env.ORIGIN || ''],
   methods: ['POST','OPTIONS']
 }));
 app.use(logger("dev"));
