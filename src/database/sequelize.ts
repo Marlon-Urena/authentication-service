48cc8386649584f7aed919a80748eeca0ebfb9dd
Marlon-Urena <anewryurena@gmail.com>
2022-01-27 12:46:23 +0000

    Enable https

--- src/database/sequelize.ts
+++ src/database/sequelize.ts
@@ -6,6 +6,10 @@ config();
 const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5433/chat-app', {
     dialect: 'postgres',
     protocol: 'postgres',
-    dialectOptions: {}
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
 });
 export default sequelize;
